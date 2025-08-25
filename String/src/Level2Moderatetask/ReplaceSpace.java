741a29fc4ff5804d4186a9e1118291f9b075024a
Savita Dhembre <dhembresavita58@gmail.com>
2025-08-25 22:39:38 +0530

    String

---
+++ String/src/Level2Moderatetask/ReplaceSpace.java
@@ -0,0 +1,15 @@
+
+//Replace all spaces with ‘-’
+package Level2Moderatetask;
+
+public class ReplaceSpace {
+	
+	public static void main(String[] args) {
+		
+		String sentence="Full stack java";
+		String result=sentence.replace(" ","-");
+		System.out.println(result);
+		
+	}
+
+}
