5372e33526c58a3793ad03df8bef781275ac41a2
Savita Dhembre <dhembresavita58@gmail.com>
2026-03-10 14:37:38 +0530

    Arithematic operation

---
+++ Functions/src/com/functions/MathematicalsModules.java
@@ -0,0 +1,31 @@
+package com.functions;
+
+public class MathematicalsModules {
+	
+	static int add(int a,int b) {
+		return a+b;
+	}
+	static int sub(int c, int d) {
+		return c-d;
+	}
+	static int mul(int e, int f) {
+		return e*f;
+	}
+	static int div (int h, int k) {
+		return h/k;
+	}
+	
+	
+	
+	public static void main(String args[]) {
+	
+		System.out.println(add(2,4));
+		System.out.println(sub(6,4));
+		System.out.println(mul(5,2));
+		System.out.println(div(10,2));
+		
+		
+		
+	}
+
+}
