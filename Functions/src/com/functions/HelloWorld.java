a4fccb53df5e81e8c3cfc9860d799460daf74789
Savita Dhembre <dhembresavita58@gmail.com>
2026-03-10 14:29:23 +0530

    function

---
+++ Functions/src/com/functions/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.functions;
+
+public class HelloWorld {
+	
+	static void print() {
+		System.out.println("Hello savita");
+	}
+	
+	
+	public static void main(String args[]) {
+		
+		print();
+	}
+
+}
